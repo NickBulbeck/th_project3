9f428fb1e8b5a284378218925f2ba8305916a0b2
NickBulbeck <bulbecknick@gmail.com>
2020-01-14 13:50:11 +0000

    First commit: comments, blank script.js file and the css reset added.

---
+++ js/script.js
@@ -0,0 +1,23 @@
+/*************************************************************************************
+
+	script.js contains all the official javascript for the Project 3 app.
+
+	Some thoughts about functions I'll need (in no particular order):
+
+	1) A date/time clash detector, with a decision on how generic and abstracted to
+		make it - that is, do I keep it local to this project where there are only
+		two days and two time slots, or do I make one that can detect an overlap 
+		between any two dates/times?
+	2) An event-handler on the entire form that runs a' the validations and stores an
+		array of valid/invalid stuff (with error messages, maybe)
+	3) A set of validating functions
+	4) A data.js file with error messages in
+	5) A way of storing and re-loading data?
+	6) Functions to hide/show the Colour stuff until T-Shirt design is selected
+
+	All this and much more!
+
+	Easter egg... not sure yet. But maybe, if you enter the name "Arthur Dent" you get
+	"you have been diverted...". Though that means not having a Douglas Adams button.
+
+**************************************************************************************/
